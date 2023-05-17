d2572e51f4bd289e1b77df4ccc81917462beb9f0
RosinskaK <rosinska.eu@gmail.com>
2023-05-17 15:33:19 +0200

    repository basics and routing

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+import Main from './components/Main.jsx';
+import SignIn from './components/SignIn.jsx';
+import SignUp from './components/SignUp.jsx';
+
+import './App.css';
+
+function App() {
+    return(
+        <Router>
+            <Routes>
+                <Route path={'/'} element={<Main />} ></Route>
+                <Route path={'/signin'} element={<SignIn />} ></Route>
+                <Route path={'/signup'} element={<SignUp />} ></Route>
+            </Routes>
+        </Router>
+    )
+
+}
+
+export default App;
