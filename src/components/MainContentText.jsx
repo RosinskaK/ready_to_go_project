4b97dc31f1ef510630391cc28568dfe463029301
RosinskaK <rosinska.eu@gmail.com>
2023-06-09 16:41:40 +0200

    main content text fix

--- src/components/MainContentText.jsx
+++ src/components/MainContentText.jsx
@@ -4,8 +4,8 @@ export default function MainContentText () {
         <div className='main-content-text-div'>
             <p className="main-content-text-line">Czas wyjazdu zbliża się wielkimi krokami, ale zanim do niego dojdzie, trzeba zająć się troszeczkę mniej przyjemnym zadaniem - pakowaniem😉</p>
             <p className="main-content-text-line">Z pomocą przychodzi checklista <span>ReadyToGo</span></p>
-            <p className="main-content-text-line">Tworząc swoją listę, pamiętaj o spakowaniu niezbędnych: leków, dokumentów (paszport i/lub dowód osobisty, bilety, dokumenty potwierdzające rezerwacje hotelowe), a także ładowarek do różnego rodzaju sprzętu elektronicznego (jak telefon czy laptop).</p>
-            <p className="main-content-text-line">Decyzja co zabrać zależy wyłącznie od Ciebe i Twoich potrzeb.</p>
+            <p className="main-content-text-line">Tworząc swoją listę, pamiętaj o spakowaniu niezbędnych: leków, dokumentów (paszport i/lub dowód osobisty, bilety, dokumenty potwierdzające rezerwacje hotelowe), a także ładowarek do różnego rodzaju sprzętu elektronicznego (jak telefon, laptop czy aparat).</p>
+            <p className="main-content-text-line">Decyzja co zabrać, zależy wyłącznie od Ciebe i Twoich potrzeb.</p>
             <p className="main-content-text-line">W procesie pakowania wspieraj się listą, odhaczaj, usuwaj i korzystaj wielokrotnie.</p>
             <p className="main-content-text-line">Kiedy będzie już pewne, że wszystko zostało spakowane, pozostanie Ci jedynie cieszyć się wyjazdem.</p>
             <h4>Udanej podróży!!</h4>
