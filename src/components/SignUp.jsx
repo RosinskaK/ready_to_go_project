6535166d34ca259948427c2042983bc4509efd7f
RosinskaK <rosinska.eu@gmail.com>
2024-02-14 15:24:50 +0100

    font upgrade

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,4 +1,4 @@
-import { useNavigate, Link} from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 import supabase from "../services/supabase.js";
 
@@ -8,63 +8,76 @@ import Footer from "./Footer.jsx";
 //-------ZAREJESTRUJ SIĘ------------------
 
 function SignUp() {
+  const navigation = useNavigate();
 
-    const navigation = useNavigate();
+  const handleSignUp = async (e) => {
+    e.preventDefault();
 
-    const handleSignUp = async (e) => {
-        e.preventDefault();
+    const { email, password, password_repeat } = e.target.elements;
 
-        const { email, password, password_repeat } = e.target.elements;
+    // if (password.value !== password_repeat.value) {
+    //     alert('Oba hasła muszą być identyczne');
+    //     return;
+    // }
 
-        // if (password.value !== password_repeat.value) {
-        //     alert('Oba hasła muszą być identyczne');
-        //     return;
-        // }
-
-        if (password.value === "" || (password.value !== password_repeat.value)) {
-            alert('Hasła nie są takie same lub puste');
-        } else if (!email.value.includes('@')) {
-            alert('Email musi posiadać znak @');
-        } else if (password.value.length <6 || password_repeat.value.length <6) {
-            alert('Hasło musi mieć co najmniej 6 znaków');
-        }
-
-        let { error } = await supabase.auth.signUp({
-            email: email.value,
-            password: password.value,
-        });
-
-        if (!error) {
-            navigation('/');
-            return;
-        }
-        console.log(error);
+    if (password.value === "" || password.value !== password_repeat.value) {
+      alert("Hasła nie są takie same lub puste");
+    } else if (!email.value.includes("@")) {
+      alert("Email musi posiadać znak @");
+    } else if (password.value.length < 6 || password_repeat.value.length < 6) {
+      alert("Hasło musi mieć co najmniej 6 znaków");
     }
 
-    return (
-        <>
-        <div className='signup-div'>
-            <div className='signup-main-container'>
-                <p className='signup-logo'>ReadyToGo</p>
-                <Welcome/>
-                <div className='signup-back'>
-                    <form onSubmit={handleSignUp} className='signup-form'>
-                        <p className='signup-sign'>Zarejestruj się</p>
-                        <input className='input-sign' id='email' placeholder='Podaj swój adres email'/>
-                        <input className='input-sign' id="password" type="password" placeholder="Podaj hasło"/>
-                        <input className='input-sign' id='password_repeat' type='password'
-                               placeholder='Potwierdź hasło'/>
-                               <p></p>
-                        <button className='sign-btn'>Zarejestruj się</button>
-                    </form>
-                    <p className='link-to-signin'>lub przejdź do logowania</p>
-                    <button className='sign-link'><Link to='/signin'>Zaloguj się</Link></button>
-                </div>
-            </div>
+    let { error } = await supabase.auth.signUp({
+      email: email.value,
+      password: password.value,
+    });
+
+    if (!error) {
+      navigation("/");
+      return;
+    }
+    console.log(error);
+  };
+
+  return (
+    <>
+      <div className="signup-div">
+        <div className="signup-main-container">
+          <p className="signup-logo">ReadyToGo</p>
+          <Welcome />
+          <div className="signup-back">
+            <form onSubmit={handleSignUp} className="signup-form">
+              <p className="signup-sign">Zarejestruj się</p>
+              <input
+                className="input-sign"
+                id="email"
+                placeholder="Podaj swój adres email"
+              />
+              <input
+                className="input-sign"
+                id="password"
+                type="password"
+                placeholder="Podaj hasło"
+              />
+              <input
+                className="input-sign"
+                id="password_repeat"
+                type="password"
+                placeholder="Potwierdź hasło"
+              />
+              <button className="sign-btn">Zarejestruj się</button>
+            </form>
+            <p className="link-to-signin">lub przejdź do logowania</p>
+            <button className="sign-link">
+              <Link to="/signin">Zaloguj się</Link>
+            </button>
+          </div>
         </div>
-        <Footer/>
-        </>
-    );
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default SignUp;
